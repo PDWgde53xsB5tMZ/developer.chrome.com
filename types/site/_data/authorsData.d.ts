ccdc703cf2b4ed59ed1cedbb3d61b9447cd710ca
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:52 +0300

    comment

--- types/site/_data/authorsData.d.ts
+++ types/site/_data/authorsData.d.ts
@@ -1,7 +1,7 @@
 /*
  * Copyright 2021 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * This file is licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -15,25 +15,42 @@
  */
 
 declare global {
+  // Interface for a single item of authors data
   export interface AuthorsDataItem {
+    // The country of the author (optional)
     country?: string;
+
+    // The GitHub username of the author (optional)
     github?: string;
+
+    // The Glitch username of the author (optional)
     glitch?: string;
+
+    // The homepage URL of the author (optional)
     homepage?: string;
+
+    // The URL of the author's image (optional)
     image?: string;
+
+    // The Twitter username of the author (optional)
     twitter?: string;
-  };
+  }
 
+  // Interface for a collection of authors data
   export interface AuthorsData {
+    // A mapping of author names to their corresponding data
     [key: string]: AuthorsDataItem;
-  };
+  }
 
+  // Interface for a single item in a virtual collection of authors
   export interface AuthorsItem extends VirtualCollectionItem, AuthorsDataItem {}
 
+  // Interface for a virtual collection of authors
   export interface Authors {
+    // A mapping of author names to their corresponding item in the virtual collection
     [key: string]: AuthorsItem;
   }
 }
 
-// empty export to keep file a module
+// Export an empty object to ensure this file is treated as a module
 export {};
