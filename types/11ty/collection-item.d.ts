83dc3e42926c796a3e6ca56a7f01e3b6ba049aeb
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:56 +0300

    comment

--- types/11ty/collection-item.d.ts
+++ types/11ty/collection-item.d.ts
@@ -1,55 +1,63 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+/**
+ * The `EleventyCollectionItem` interface extends the built-in `CollectionItem` interface in Eleventy,
+ * adding several properties that are specific to the user's project.
  */
-
 declare global {
-  export interface EleventyCollectionItem {
+  /**
+   * This interface defines the shape of an item in an Eleventy collection.
+   */
+  export interface EleventyCollectionItem extends CollectionItem {
     /**
-     * The full path to the source input file (including the path to the input directory)
+     * The full path to the source input file (including the path to the input directory).
+     * This property is useful for permalinks and other file-related operations.
      */
     inputPath: string;
+
     /**
      * Mapped from the input file name, useful for permalinks. Read more about [`fileSlug`](https://www.11ty.dev/docs/data-eleventy-supplied/#fileslug).
+     * This property is used to generate the URL for this piece of content.
      */
     fileSlug: string;
+
     /**
-     * Mapped from the input file path
+     * Mapped from the input file path. This property is useful for organizing content into directories.
      */
     filePathStem: string;
+
     /**
-     * The full path to the output file to be written for this content
+     * The full path to the output file to be written for this content.
+     * This property is used to generate the final output files for the user's site.
      */
     outputPath: string;
+
     /**
      * URL used to link to this piece of content.
+     * This property is used to generate links to the user's content.
      */
     url: string;
+
     /**
      * The resolved date used for sorting. Read more about [Content Dates](https://www.11ty.dev/docs/dates/).
+     * This property is used to sort content by date.
      */
     date: Date;
+
     /**
-     * All data for this piece of content (includes any data inherited from layouts)
+     * All data for this piece of content (includes any data inherited from layouts).
+     * This property is used to access data associated with this piece of content.
      */
     data: EleventyData;
+
     /**
      * The rendered content of this template. This does not include layout wrappers.
+     * This property is used to access the content of this piece of content.
      */
     templateContent: string;
+
     /**
      * @UNDOCUMENTED
+     *
+     * This property is not documented in the Eleventy documentation, and its purpose is unclear.
      */
     template: {
       [key: string]: TODO;
@@ -57,5 +65,5 @@ declare global {
   }
 }
 
-// empty export to keep file a module
+// This empty export is used to ensure that this file is treated as a module.
 export {};
