933192db871d768652bed895b3ad6b5bc83f497c
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:38 +0300

    comment

--- tests/rollup.config.js
+++ tests/rollup.config.js
@@ -1,14 +1,10 @@
-import originalConfig from '../rollup.config';
-
-const inputs = [
-  'site/_js/web-components/enhanced-select/_enhanced-select',
-  'site/_js/web-components/load-more/_load-more',
-];
-
-const configs = inputs.map(input => {
-  return Object.assign({}, originalConfig(), {
-    input: input,
-  });
-});
-
-export default configs;
+[
+  {
+    input: 'site/_js/web-components/enhanced-select/_enhanced-select',
+    // ...other properties from originalConfig
+  },
+  {
+    input: 'site/_js/web-components/load-more/_load-more',
+    // ...other properties from originalConfig
+  },
+]
