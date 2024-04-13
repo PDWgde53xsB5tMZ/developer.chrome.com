1ab6f6c7860c6c6312db6db1c769b2bf2a8ec134
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:09 +0300

    comment

--- site/ja/blog/blog.11tydata.js
+++ site/ja/blog/blog.11tydata.js
@@ -1,3 +1,5 @@
+// This module exports an object with a single property "type" set to the string value "blogPost".
+// This can be used to identify the type of object being worked with in other parts of the application.
 module.exports = {
   type: 'blogPost',
 };
