10d75f561861563cd41e87a5cbe82d9a6cbfc2bc
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:28 +0300

    comment

--- gulp-tasks/stageGitHub/announceDeploymentFinished.js
+++ gulp-tasks/stageGitHub/announceDeploymentFinished.js
@@ -19,16 +19,26 @@
  * staging build status.
  */
 
+// Import required modules
 const {requestGitHubApi} = require('./lib/gitHubApi');
 const {getDeploymentType} = require('./lib/getDeploymentType');
 const {isGoogleCloudBuild} = require('./lib/isGoogleCloudBuild');
 const {updateStickyComment} = require('./lib/updateStickyComment');
 
+/**
+ * Retrieves the list of changed pages in the pull request.
+ * @param {number} prNumber - The pull request number.
+ * @param {string} baseUrl - The base URL for the preview environment.
+ * @returns {string} The markdown content for the changed pages.
+ */
 async function getChangedPages(prNumber, baseUrl) {
+  // Fetch the list of changed files in the pull request
   const {data: changedFiles} = await requestGitHubApi(
     'GET',
     `pulls/${prNumber}/files?per_page=100`
   );
+
+  // Filter the changed files to only include the pages
   const changedPages = changedFiles
     .filter(file => {
       const filePath = file.filename;
@@ -36,12 +46,14 @@ async function getChangedPages(prNumber, baseUrl) {
     })
     .map(file => {
       const filePath = file.filename;
+      // Construct the URL for the changed page
       return `- ${baseUrl}${filePath
         .replace('site/', '')
         .replace(/^en\//, '')
         .replace(/index.md$/, '')}`;
     });
 
+  // Return the markdown content for the changed pages
   if (changedPages.length) {
     return (
       '\n\n' +
@@ -53,13 +65,18 @@ async function getChangedPages(prNumber, baseUrl) {
   return '';
 }
 
+/**
+ * Announces the deployment finished in the GitHub comment.
+ */
 async function announceDeploymentFinished() {
+  // Check if the build is triggered by Google Cloud Build
   isGoogleCloudBuild();
 
   const prNumber = process.env.PR_NUMBER;
   const commitSha = process.env.COMMIT_SHA;
   const deploymentType = await getDeploymentType();
 
+  // Exit early if there's no deployment type
   if (!deploymentType) {
     await updateStickyComment(
       prNumber,
@@ -68,9 +85,11 @@ async function announceDeploymentFinished() {
     return;
   }
 
+  // Construct the base URL for the preview environment
   const baseUrl = `https://pr-${prNumber}-${deploymentType}-dot-dcc-staging.uc.r.appspot.com/`;
   const changedPages = await getChangedPages(prNumber, baseUrl);
 
+  // Update the GitHub comment with the deployment status and the changed pages
   await updateStickyComment(
     prNumber,
     `:white_check_mark: Preview (${deploymentType}) for commit ${commitSha} available at ${baseUrl}.` +
@@ -78,6 +97,7 @@ async function announceDeploymentFinished() {
   );
 }
 
+// Export the announceDeploymentFinished function for external use
 module.exports = {
   announceDeploymentFinished,
 };
