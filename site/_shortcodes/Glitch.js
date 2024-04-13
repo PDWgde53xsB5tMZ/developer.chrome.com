bd833fbca0cc7c7e96be1167cd661afc6ab8c773
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:43 +0300

    comment

--- site/_shortcodes/Glitch.js
+++ site/_shortcodes/Glitch.js
@@ -1,28 +1,14 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright information and license details.
 
 const {html} = require('common-tags');
 const {IFrame} = require('./IFrame');
 
 /**
- * Validates allow sources are an array and lower case.
- * If allow sources are a string, it will be split by the `;` character.
+ * Converts `allow` sources to an array and lowercases them. If `allow` sources
+ * are a string, it will split it by the `;` character.
  *
- * @param {string|string[]} s
- * @returns {string[]}
+ * @param {string|string[]} s - The `allow` sources.
+ * @returns {string[]} The converted `allow` sources.
  */
 function expandAllowSource(s) {
   if (typeof s === 'string') {
@@ -34,8 +20,8 @@ function expandAllowSource(s) {
 /**
  * Generates HTML for a Glitch embed as a string.
  *
- * @param {GlitchArgs} args  Named arguments
- * @return {string}
+ * @param {GlitchArgs} args - Named arguments.
+ * @return {string} The generated HTML.
  */
 const Glitch = args => {
   const defaultAllow = [
@@ -57,17 +43,18 @@ const Glitch = args => {
     ...args,
   };
 
+  // Validation to ensure an `id` is provided.
   if (!glitchProps.id) {
     throw new Error('No `id` provided to Glitch shortcode.');
   }
 
-  // searchParams are not directly set on `url` here, as URL would move the
-  // query string before the #, which would break for Glitch
+  // Creating the URL for the Glitch embed.
   const url = new URL(`https://glitch.com/embed/#!/embed/${glitchProps.id}`);
   const searchParams = new URLSearchParams();
   searchParams.set('attributionHidden', 'true');
   searchParams.set('sidebarCollapsed', 'true');
 
+  // Adding the `path` and `previewSize` to the URL search parameters.
   if (glitchProps.path) {
     searchParams.set('path', glitchProps.path);
   }
@@ -75,10 +62,12 @@ const Glitch = args => {
     searchParams.set('previewSize', String(glitchProps.previewSize));
   }
 
+  // Combining the default and provided `allow` sources.
   const allow = Array.from(
     new Set([...defaultAllow, ...expandAllowSource(glitchProps.allow)])
   ).join('; ');
 
+  // Returning the generated HTML for the Glitch embed.
   return html`
     <div
       class="glitch-embed-wrap"
@@ -93,4 +82,5 @@ const Glitch = args => {
   `.replace(/\s\s+/g, ' ');
 };
 
+// Exporting the `Glitch` function.
 module.exports = {Glitch};
