61fdeddbae32847a4ff1d09f5b8f615733b309fb
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:42 +0300

    comment

--- tests/site/_js/utils/truncate-string.js
+++ tests/site/_js/utils/truncate-string.js
@@ -1,30 +1,36 @@
+// Importing the 'ava' testing library to test the 'truncate-string' utility function
 const test = require('ava');
+
+// Importing the 'truncate-string' utility function from '../../../../site/_js/utils/truncate-string'
 const truncateString = require('../../../../site/_js/utils/truncate-string');
 
+// Test case to check if 'truncate-string' does not break words when truncating
 test('truncate-string does not break words', t => {
+  // Asserting that truncating 'Lorem Ipsum is simply dummy text' to 17 characters returns 'Lorem Ipsum is'
   t.assert(
     truncateString('Lorem Ipsum is simply dummy text', 17) === 'Lorem Ipsum is'
   );
 });
 
+// Test case to check if 'truncate-string' can trim to the first word
 test('truncate-string can trim to the first word', t => {
+  // Asserting that truncating 'Lorem Ipsum is simply dummy text' to 4 characters returns 'Lorem'
   t.assert(truncateString('Lorem Ipsum is simply dummy text', 4) === 'Lorem');
 });
 
+// Test case to check if 'truncate-string' can trim to the last word
 test('truncate-string can trim to the last word', t => {
+  // Asserting that truncating 'Lorem Ipsum is simply dummy text' to 32 characters returns 'Lorem Ipsum is simply dummy text'
   t.assert(
     truncateString('Lorem Ipsum is simply dummy text', 32) ===
       'Lorem Ipsum is simply dummy text'
   );
 });
 
+// Test case to check if 'truncate-string' can handle empty strings
 test('truncate-string can handle empty strings', t => {
+  // Asserting that truncating an empty string to any length returns an empty string
   t.assert(truncateString('', 12) === '');
 });
 
-test('truncate-string does not truncate if max length is greater than the string length', t => {
-  t.assert(
-    truncateString('Lorem Ipsum is simply dummy tex', 500) ===
-      'Lorem Ipsum is simply dummy tex'
-  );
-});
+// Test case to check if 'trunc
