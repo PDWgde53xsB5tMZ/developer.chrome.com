e3274e452abc6af1aacca873cebe71bc39e8a629
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:19 +0300

    comment

--- site/_data/chrome.js
+++ site/_data/chrome.js
@@ -1,7 +1,7 @@
 /*
  * Copyright 2020 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * This file is licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,26 +14,37 @@
  * limitations under the License.
  */
 
+// Importing the 'fs' module for file system operations and the 'path' module
+// for handling file paths.
 const fs = require('fs');
 const path = require('path');
 
 /**
- * Generates Chrome version information.
+ * This module exports an asynchronous function that generates Chrome
+ * version information.
  */
 module.exports = async function buildVersionInformation() {
+  // The path to the JSON file containing Chrome release data.
   const chromeReleaseDataFile = path.join(
     __dirname,
     '../../external/data/chrome-release.json'
   );
+
+  // Reading the JSON file and parsing it into a JavaScript object.
   const data = /** @type {{channels: Object<string, ChromeReleaseData>}} */ (
     JSON.parse(fs.readFileSync(chromeReleaseDataFile, 'utf-8'))
   );
 
+  // Iterating over each channel in the 'channels' object of the data.
   for (const channel in data.channels) {
-    const o = data.channels[channel];
-    // JSON-encoding flattens this to a string, so reconstruct the Date.
-    o.stableDate = new Date(o.stableDate);
+    const channelData = data.channels[channel];
+
+    // Reconstructing the 'stableDate' property as a JavaScript Date object,
+    // as JSON encoding flattens this to a string.
+    channelData.stableDate = new Date(channelData.stableDate);
   }
 
+  // Returning the data object after updating the 'stableDate' properties.
   return data;
 };
+
