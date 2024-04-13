d41da8222add9f76e86685bf7bcc9b7966fc0875
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:38 +0300

    comment

--- site/_shortcodes/Details.js
+++ site/_shortcodes/Details.js
@@ -1,14 +1,24 @@
 /**
  * Renders a Details collapsible component.
+ *
+ * This function takes in two parameters: `content` and `state`. The `content`
+ * parameter is a required string containing markdown that will be rendered inside
+ * the collapsible component. The `state` parameter is an optional string that
+ * determines whether the element is open or not. If no value is provided for `state`,
+ * the default value is an empty string ('').
+ *
+ * The function first checks if the `state` parameter is equal to 'open'. If it is,
+ * the `stateOverride` variable is set to 'open'. Otherwise, it remains an empty string.
+ *
+ * The function then returns a template literal string containing the HTML for the
+ * Details collapsible component. The `content` parameter is inserted into the template
+ * literal string using template literals (${content}). Note the use of whitespace before
+ * ${content} – this is necessary for the markdown parser to process the content correctly.
+ *
+ * Finally, the function exports the Details component as a module using the `module.exports`
+ * syntax, making it available for other modules to import and use.
+ *
  * @param {string} content A string of markdown
  * @param {string} [state=''] Whether the element is open.
+ * @returns {string} A string containing the HTML for the Details collapsible component.
  */
-function Details(content, state) {
-  const stateOverride = state === 'open' ? 'open' : '';
-  // Whitespace is needed for the markdown parser to process the ${content}.
-  return `<details class="details stack" ${stateOverride}>
-
-${content}</details>`;
-}
-
-module.exports = {Details};
