0b24a6f6c9b12c69ccdc8eb14137cdcfa4d3a7d3
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:57:27 +0300

    comment

--- site/en/authors/author/author.11tydata.js
+++ site/en/authors/author/author.11tydata.js
@@ -1,11 +1,11 @@
 /*
  * Copyright 2021 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This JavaScript file is licensed under the Apache License, Version 2.0 
+ * (the "License"); you may not use this file except in compliance with 
+ * the License. You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,13 +14,20 @@
  * limitations under the License.
  */
 
-const {individual} = require('../../../_utils/individual');
-const {locale} = require('../../en.11tydata');
+// Importing the 'individual' utility function from '../../../_utils/individual'
+const { individual } = require('../../../_utils/individual');
 
+// Importing the 'locale' object from '../../en.11tydata'
+const { locale } = require('../../en.11tydata');
+
+// The 'pagination' object is being exported with a single property 'before'
 module.exports = {
   pagination: {
+    // The 'before' property is a function that takes an array of authors as an argument
     before: authors => {
+      // The 'individual' utility function is called with the 'authors' array and 'locale' object as arguments
       return individual(authors, locale);
     },
   },
 };
+
