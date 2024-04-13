1a0e9a24d5ee08410135b9e9a03bb9548f1c8086
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:52 +0300

    comment

--- types/11ty/collection-object.d.ts
+++ types/11ty/collection-object.d.ts
@@ -1,47 +1,35 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// This is a global interface that extends the EleventyCollectionObject interface
 declare global {
+  // This interface defines the shape of an object that contains a collection of Eleventy content items
   export interface EleventyCollectionObject {
+    // This is a reference to all items in the collection
     all: EleventyCollectionItem[];
+
+    // This is a reference to all items in the collection, in the order they were added
     items: EleventyCollectionItem[];
+
+    // This is the length of the collection (i.e., the number of items it contains)
     length: number;
+
+    // This is an index signature that allows you to access items in the collection by tag name
     [tag: key]: EleventyCollectionItem[];
-    /**
-     * Unsorted items (in whatever order they were added)
-     */
+
+    // This method returns all items in the collection, in whatever order they were added
     getAll: () => EleventyCollectionItem[];
-    /**
-     * Use the default sorting algorithm (ascending by date, filename tiebreaker)
-     */
+
+    // This method returns all items in the collection, sorted in ascending order by date, with a filename tiebreaker
     getAllSorted: () => EleventyCollectionItem[];
-    /**
-     * Get only content that matches a tag
-     */
+
+    // This method returns all items in the collection that have a specific tag
     getFilteredByTag: (tagName: string) => EleventyCollectionItem[];
-    /**
-     * Get only content that matches a tag
-     */
+
+    // This method returns all items in the collection that have specific tags
     getFilteredByTags: (...tagNames: string[]) => EleventyCollectionItem[];
-    /**
-     * Filter source file names using a glob
-     */
+
+    // This method returns all items in the collection that match a specific glob pattern
     getFilteredByGlob: (glob: string) => EleventyCollectionItem[];
   }
 }
 
-// empty export to keep file a module
+// This empty export is necessary to keep the file a module
 export {};
