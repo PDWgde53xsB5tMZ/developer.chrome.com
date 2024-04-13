c7774f3ea43abb1b5aff8e734d96b2bf6c01ea9c
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:59 +0300

    comment

--- version-check.js
+++ version-check.js
@@ -4,38 +4,45 @@
  * is kicked off ('deploy.yml').
  */
 
+// Importing required modules
 const {default: fetch} = require('node-fetch');
 const {ErrorReporting} = require('@google-cloud/error-reporting');
 const getVersion = require('./tools/version');
 const {CloudBuildClient} = require('@google-cloud/cloudbuild');
 
-const client = new CloudBuildClient();
+// Initializing ErrorReporting and CloudBuildClient
 const errors = new ErrorReporting();
-const ERROR_MESSAGE = 'NOT FOUND';
+const client = new CloudBuildClient();
 
-// This is the trigger ID of "deploy-public" for "chrome-apps-doc".
+// Setting up constants
+const ERROR_MESSAGE = 'NOT FOUND';
 const deployTriggerId = 'eefc1e51-0e94-48e1-a1be-76da08f9a0b6';
 
 /**
- * @returns {Promise<string>}
+ * @returns {Promise<string>} Returns the deployed version as a string.
  */
 const getDeployedVersion = () => {
+  // Fetching the deployed version from the URL
   return fetch('https://developer.chrome.com/site-version')
     .then(res => (res.ok ? res.text() : ERROR_MESSAGE))
     .catch(() => ERROR_MESSAGE);
 };
 
 (async () => {
+  // Getting the current and deployed versions
   const deployedVersion = await getDeployedVersion();
   const currentVersion = getVersion();
 
+  // Logging the current and deployed versions
   console.log(`Current version: ${currentVersion}`);
   console.log(`Deployed version: ${deployedVersion}`);
 
+  // Checking if the deployed version is not found
   if (deployedVersion === ERROR_MESSAGE) {
     errors.report('Deployed commit SHA not found');
   }
 
+  // Comparing the current and deployed versions
   if (deployedVersion === currentVersion) {
     console.log(
       'The current and deployed versions are the same, not continuing build.'
@@ -43,18 +50,21 @@ const getDeployedVersion = () => {
     return;
   }
 
+  // Comparing the current and deployed versions and kicking off deploy build
   console.log(
     'The current and deployed versions are different, kicking off deploy build.'
   );
 
-  // Check if there are any existing builds.
+  // Checking if there are any existing builds
   const ret = client.listBuildsAsync({
     projectId: process.env.PROJECT_ID,
     pageSize: 1,
     filter: `trigger_id="${deployTriggerId}" AND (status="WORKING" OR status="QUEUED")`,
   });
+
   let activeBuild = false;
-  // eslint-disable-next-line no-unused-vars
+
+  // If there is an active or queued build, not starting another
   for await (const _build of ret) {
     activeBuild = true;
     break;
@@ -66,9 +76,8 @@ const getDeployedVersion = () => {
     return;
   }
 
+  // Running the build trigger
   try {
-    // This just waits for the build to be kicked off, not for its completion (it
-    // returns a LROperation).
     await client.runBuildTrigger({
       projectId: process.env.PROJECT_ID,
       triggerId: deployTriggerId,
