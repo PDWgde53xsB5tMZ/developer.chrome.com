8a77e191454ad6390ee917a21bd16f2a5c18294f
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:04 +0300

    comment

--- site/es/tags/tag/tag.11tydata.js
+++ site/es/tags/tag/tag.11tydata.js
@@ -1,20 +1,10 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Import the `locale` object from the `es.11tydata` module.
 const {locale} = require('../../es.11tydata');
+
+// Import the `tag11tyData` utility function from the `tag-11tydata` module.
+// This function generates 11ty data for a given tag.
 const tag11tyData = require('../../../_utils/tag-11tydata');
 
+// Export the `tag11tyData` function, passing in the `locale` object as an argument.
+// This will generate 11ty data for the specified locale.
 module.exports = tag11tyData(locale);
