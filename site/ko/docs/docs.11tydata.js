fbe65c2c097d7ca2733aa1e008c592b9572a9895
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:37 +0300

    comment

--- site/ko/docs/docs.11tydata.js
+++ site/ko/docs/docs.11tydata.js
@@ -1,3 +1,5 @@
+// This module exports an object with a single property named `type` set to the string `'doc'`.
+// This type of object is commonly used in JavaScript to represent a document or a piece of structured data.
 module.exports = {
   type: 'doc',
 };
