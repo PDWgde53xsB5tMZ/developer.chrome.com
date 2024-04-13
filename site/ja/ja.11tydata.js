4d5e1c0a40e3fa4fb03e479a24a15e8dc341335b
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:30 +0300

    comment

--- site/ja/ja.11tydata.js
+++ site/ja/ja.11tydata.js
@@ -1,3 +1,8 @@
+// This module exports an object with a single property `locale` set to `'ja'`.
+// The `locale` property specifies the Japanese language as the preferred locale.
+// This configuration is often used in internationalization libraries to format
+// dates, numbers, and other locale-specific data.
 module.exports = {
   locale: 'ja',
 };
+
