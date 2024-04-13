ac880ff438fa22e09cfde2e2be1089c1188f77c2
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:46 +0300

    comment

--- site/ko/ko.11tydata.js
+++ site/ko/ko.11tydata.js
@@ -1,3 +1,5 @@
+// This module exports an object with a single property `locale` set to the string value 'ko'.
+// The `locale` property specifies the language of the application to be Korean.
 module.exports = {
   locale: 'ko',
 };
