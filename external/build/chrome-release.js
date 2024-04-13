82887bd7642426b0aff64cfc4ddbe4a45847ef40
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:13 +0300

    comment

--- external/build/chrome-release.js
+++ external/build/chrome-release.js
@@ -1,16 +1,27 @@
 /**
- * @fileoverview Fetches Chrome release data and writes to storage.
+ * @fileoverview This file is responsible for fetching Chrome release data and storing it in a local JSON file.
  */
 
-const buildVersionInformation = require('./lib/chrome-release.js');
-const fs = require('fs');
-const path = require('path');
+// Importing the required modules
+const buildVersionInformation = require('./lib/chrome-release.js'); // For fetching Chrome release data
+const fs = require('fs'); // For interacting with the file system
+const path = require('path'); // For handling file paths
 
+/**
+ * Async function run()
+ * 
+ * @description - The main function that fetches Chrome release data and writes it to a local JSON file.
+ */
 async function run() {
+  // Fetching Chrome release data
   const versionInformation = await buildVersionInformation();
 
+  // Defining the target file path to store the Chrome release data
   const targetFile = path.join(__dirname, '../data/chrome-release.json');
+
+  // Writing the fetched data to the target file in JSON format
   fs.writeFileSync(targetFile, JSON.stringify(versionInformation));
 }
 
+// Calling the run() function to execute the main functionality
 run();
