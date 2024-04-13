f6213f2d385ab897f0c089871f0fa0ed73357744
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:57:45 +0300

    comment

--- external/build/workbox-types.js
+++ external/build/workbox-types.js
@@ -2,6 +2,7 @@
  * @fileoverview Fetches and preprocesses types from Workbox.
  */
 
+// Import required modules
 const dtsParse = require('./lib/dts-parse.js');
 const tmp = require('tmp');
 const fs = require('fs');
@@ -9,7 +10,7 @@ const mvdir = require('mvdir');
 const path = require('path');
 const childProcess = require('child_process');
 
-// Just contains SW packages for now.
+// Array of Workbox packages to fetch
 const workboxPackages = [
   'workbox-background-sync',
   'workbox-broadcast-update',
@@ -37,9 +38,11 @@ async function fetchAndPrepare(packages, targetDir) {
   /** @type {childProcess.ExecFileSyncOptions} */
   const options = {cwd: targetDir, stdio: 'inherit'};
 
+  // Initialize a new npm project and install the given packages
   childProcess.execFileSync('npm', ['init', '--yes'], options);
   childProcess.execFileSync('npm', ['install', ...packages], options);
 
+  // Get the version of each package and log it
   const versionData = Object.fromEntries(
     packages.map(name => {
       const p = path.join(targetDir, 'node_modules', name, 'package.json');
@@ -53,6 +56,7 @@ async function fetchAndPrepare(packages, targetDir) {
 }
 
 async function run() {
+  // Create a temporary directory and install the packages there
   const t = tmp.dirSync();
   try {
     // webpack is a peerDependency of workbox-webpack-plugin, and needs to be
@@ -63,6 +67,7 @@ async function run() {
     });
     await fetchAndPrepare(workboxPackages, t.name);
 
+    // Initialize an array to store the source files
     const sources = [];
     for (const packageName of workboxPackages) {
       const packagePath = path.join(t.name, 'node_modules', packageName);
@@ -70,19 +75,24 @@ async function run() {
       const packageJson = JSON.parse(fs.readFileSync(packageJsonPath, 'utf-8'));
       const typesPathParsed = path.parse(packageJson.types || 'index.d.ts');
 
+      // If the types file is not at the top level, move it there
       const isNotTopLevel = !!typesPathParsed.dir;
       if (isNotTopLevel) {
         const typesOutputPath = path.join(packagePath, typesPathParsed.dir);
         await mvdir(typesOutputPath, packagePath, {overwrite: true});
       }
 
+      // Add the source file to the array
       sources.push(path.join(packagePath, 'index.d.ts'));
     }
 
+    // Parse the source files and generate the definitions
     const defs = await dtsParse({sources, mode: 'workbox'});
+    // Write the definitions to a JSON file
     const outputFile = path.join(__dirname, '../data/workbox-types.json');
     fs.writeFileSync(outputFile, JSON.stringify(defs, undefined, 2));
   } finally {
+    // Clean up the temporary directory
     fs.rmSync(t.name, {recursive: true});
   }
 }
