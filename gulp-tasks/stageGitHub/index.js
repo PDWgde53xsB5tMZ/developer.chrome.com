31a97d6fba3bda1cfd184cc4048f9e7a1923d61c
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:59 +0300

    comment

--- gulp-tasks/stageGitHub/index.js
+++ gulp-tasks/stageGitHub/index.js
@@ -1,13 +1,24 @@
+// Import required functions from their respective modules.
 const {announceDeploymentStart} = require('./announceDeploymentStart');
 const {determineDeploymentType} = require('./determineDeploymentType');
 const {buildStaticSite} = require('./buildStaticSite');
 const {announceDeploymentFinished} = require('./announceDeploymentFinished');
 const {cleanUpGoogleCloud} = require('./cleanUpGoogleCloud');
 
+// Export all required functions as an object, making them available for use in other modules.
 module.exports = {
+  // announceDeploymentStart: This function announces the start of a deployment.
   announceDeploymentStart,
+  
+  // determineDeploymentType: This function determines the type of deployment.
   determineDeploymentType,
+  
+  // buildStaticSite: This function builds the static site.
   buildStaticSite,
+  
+  // announceDeploymentFinished: This function announces the finish of a deployment.
   announceDeploymentFinished,
+  
+  // cleanUpGoogleCloud: This function cleans up any remaining resources on Google Cloud.
   cleanUpGoogleCloud,
 };
