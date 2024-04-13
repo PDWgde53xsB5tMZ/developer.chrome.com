a13d41910bb3aed3c83c5b74ed6e49c1ef75f5b1
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:45 +0300

    comment

--- tests/site/_transforms/dom-transformer.js
+++ tests/site/_transforms/dom-transformer.js
@@ -1,10 +1,14 @@
+// Importing the 'ava' testing library and the 'domTransformer' module.
 const test = require('ava');
 const domTransformer = require('../../../site/_transforms/dom-transformer');
 
+// The first test case checks if the 'domTransformer' ignores the content when there is no outputPath.
 test("ignores content if there's no outputPath", async t => {
+  // Defining the content string and the expected output string.
   const content = '<a href="/en/foo"></a>';
   const expected = '<a href="/en/foo"></a>';
 
+  // Testing the 'domTransformer' with different falsy values for outputPath.
   let outputPath = false;
   let actual = await domTransformer(content, outputPath);
   t.assert(actual === expected);
@@ -18,10 +22,13 @@ test("ignores content if there's no outputPath", async t => {
   t.assert(actual === expected);
 });
 
+// The second test case checks if the 'domTransformer' ignores the content when the locale is invalid.
 test('ignores content if the locale is invalid', async t => {
+  // Defining the content string and the expected output string.
   const content = '<a href="/en/foo"></a>';
   const expected = '<a href="/en/foo"></a>';
 
+  // Testing the 'domTransformer' with different invalid outputPath values.
   let outputPath = '/xx/foo.html';
   let actual = await domTransformer(content, outputPath);
   t.assert(actual === expected);
@@ -30,3 +37,4 @@ test('ignores content if the locale is invalid', async t => {
   actual = await domTransformer(content, outputPath);
   t.assert(actual === expected);
 });
+
