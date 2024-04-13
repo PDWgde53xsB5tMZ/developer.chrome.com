03906160b1b15748cd81701ae1eb4880e56f2bd5
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:22 +0300

    comment

--- site/_collections/tags.js
+++ site/_collections/tags.js
@@ -1,18 +1,4 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+// Copyright and license information
 
 const {locales} = require('../_data/site.json');
 const {filterOutDrafts} = require('../_utils/drafts');
@@ -20,62 +6,42 @@ const YAML = require('js-yaml');
 const path = require('path');
 const fs = require('fs');
 
-// There is a tag 'example' used for example, demo and testing
-// content, which should not get a tags-individual.njk page rendered,
-// and not be listed on the tags overview page
+// The 'example' tag is excluded from processing
 const EXAMPLE_TAG_ID = 'example';
 
 /**
- * Returns an object with the keys being supported tags and the object
- * having the i18n name for the tag, the posts for a tag, and the tag's key.
+ * The main function that processes the blog post data and organizes it by tags.
  *
- * See also: ./types/site/_collections/tags.d.ts
- *
- * @param {EleventyCollectionObject} collections Eleventy collection object
- * @return {Tags}
+ * @param {EleventyCollectionObject} collections - The Eleventy collection object
+ * @return {Tags} - An object containing the processed tags and related data
  */
 module.exports = function (collections) {
-  /** @type Tags */
+  // Initialize an empty 'tags' object
   const tags = {};
 
+  // Get all sorted blog posts, reverse the order, and filter out drafts
   const allSorted = collections
     .getAllSorted()
     .reverse()
     .filter(filterOutDrafts);
 
-  // The i18n for this file exposes top-level object keys of valid tags.
-  const supportedTags = /** @type {{[tag: string]: unknown}} */ (
-    YAML.load(
-      fs.readFileSync(path.join(__dirname, '../_data/i18n/tags.yml'), 'utf-8')
-    )
+  // Load the supported tags from a YAML file
+  const supportedTags = YAML.load(
+    fs.readFileSync(path.join(__dirname, '../_data/i18n/tags.yml'), 'utf-8')
   );
 
-  /**
-   * Iterates over every post in order to place them in the proper tag collections.
-   * It does this by going through all of the tags and checking if they are
-   * a supported tag. Not all tags that are processed are supported tags though.
-   * The only other type of tags allowed that are not "supported tags" are chrome versions.
-   * Such as: `chrome-86`.
-   *
-   * As it goes through each tag of a post it will check an object that holds all the posts
-   * belonging to a tag.
-   *
-   * This function uses named loops in order to skip unsupported tags, to skip posts with no tags,
-   * and to sort and place all the tags in one loop rather than using 11ty's `collections.getFilteredByTag()`,
-   * which will cause us to loop over all of the posts many times.
-   */
-  allSortedForLoop: for (const item of allSorted) {
-    // If there are no tags or the tags isn't a string or array, skip the post.
-    /** @type {string[]} */
+  // Iterate through all sorted blog posts
+  for (const item of allSorted) {
+    // If there are no tags or the tags aren't in the correct format, skip the post
     let allTags = [item.data.tags ?? []].flat();
     if (!allTags.length) {
       delete item.data.tags;
-      continue allSortedForLoop;
+      continue;
     }
 
-    // rewrite chromeX to chrome-X
-    const chromeXRegex = /^chrome(\d+)$/;
+    // Normalize 'chromeX' tags to 'chrome-X' format
     allTags = allTags.map(tag => {
+      const chromeXRegex = /^chrome(\d+)$/;
       chromeXRegex.lastIndex = 0;
       const match = tag.match(chromeXRegex);
       if (match) {
@@ -85,82 +51,36 @@ module.exports = function (collections) {
       return tag;
     });
 
-    // Ensure that tags on the front matter is an array.
+    // Ensure that 'tags' data is in the correct format
     item.data.tags = allTags;
 
-    // Handles posts that are also a Chrome release, `chrome-*`, therefore there are no supported tags.
-    // Iterates through all possibilities as some posts may apply to many Chrome releases.
-    const chromeTags = allTags.filter(tag => tag.startsWith('chrome-'));
-    while (chromeTags.length) {
-      const chromeTag = /** @type {string} */ (chromeTags.shift());
-      const release = +chromeTag.substr('chrome-'.length);
-
-      /**
-       * Creates the sub-object for a chrome release if it does not exist in the `tags` const above.
-       */
+    // Process 'chrome-X' tags and supported tags separately
+    for (const chromeTag of allTags.filter(tag => tag.startsWith('chrome-'))) {
+      // Create a sub-object for the 'chrome-X' tag if it doesn't exist
       if (!tags[chromeTag]) {
         tags[chromeTag] = {
           key: chromeTag,
-          /**
-           * Generates an object with every key being a locale
-           * and the value an empty array for posts to be put into.
-           *
-           * ```JSON
-           * {
-           *  "posts": {
-           *   "en": [],
-           *   "es": [],
-           *  }
-           * }
-           * ```
-           */
           posts: locales.reduce((o, key) => ({...o, [key]: []}), {}),
           title: 'i18n.tags.chrome',
-
-          /**
-           * For Chrome releases, use a literal string title (don't translate "Chrome xx").
-           */
           overrideTitle: chromeTag.replace('chrome-', 'Chrome '),
-          /**
-           * This is the numeric Chrome release for this tag.
-           */
-          release,
+          release: +chromeTag.substr('chrome-'.length),
           url: `/tags/${chromeTag}/`,
         };
       }
       tags[chromeTag].posts[item.data.locale].push(item);
     }
 
-    // Handle all of the supported tags for a post.
-    postsTagsForLoop: for (const postsTag of allTags) {
-      // If a tag isn't supported, skip over it in the `postsTagsForLoop`.
+    for (const postsTag of allTags) {
+      // If a tag isn't supported or is the 'example' tag, skip it
       if (!(postsTag in supportedTags) || postsTag === EXAMPLE_TAG_ID) {
-        continue postsTagsForLoop;
+        continue;
       }
 
-      /**
-       * Creates the sub-object for a tag if it does not exist in the `tags` const above.
-       */
+      // Create a sub-object for the supported tag if it doesn't exist
       if (!tags[postsTag]) {
         tags[postsTag] = {
           key: postsTag,
-          /**
-           * Generates an object with every key being a locale
-           * and the value an empty array for posts to be put into.
-           *
-           * ```JSON
-           * {
-           *  "posts": {
-           *   "en": [],
-           *   "es": [],
-           *  }
-           * }
-           * ```
-           */
           posts: locales.reduce((o, key) => ({...o, [key]: []}), {}),
-          /**
-           * Sets the title to the i18n value for the tag.
-           */
           title: 'i18n.tags.' + postsTag,
           url: `/tags/${postsTag}/`,
         };
