3b509927a97badf9dbdba9abd35f0e7adedb364e
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:57:53 +0300

    comment

--- site/_js/styleguide.js
+++ site/_js/styleguide.js
@@ -1,7 +1,7 @@
 /*
  * Copyright 2022 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
+ * This file is licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
@@ -14,16 +14,25 @@
  * limitations under the License.
  */
 
+// Importing custom web components 'enhanced-select' and 'checkbox-group'
 import './web-components/enhanced-select';
 import './web-components/checkbox-group';
+
+// Importing 'TagPillList' web component from './web-components/tag-pill-list'
 import {TagPillList} from './web-components/tag-pill-list';
 
+// Selecting the 'tag-pill-list' element with id 'tag-pill-demo'
 const tagPillComponent = document.querySelector('#tag-pill-demo tag-pill-list');
+
+// Selecting the button element within the 'tag-pill-demo' element
 const tagPillButton = document.querySelector('#tag-pill-demo button');
 
+// Adding a click event listener to the button
 tagPillButton?.addEventListener('click', () => {
+  // Checking if the selected 'tagPillComponent' is an instance of 'TagPillList'
   if (!(tagPillComponent instanceof TagPillList)) return;
 
+  // Adding a new tag pill with key 'addition' and value 'Addition' to the existing items
   tagPillComponent.items = [
     ...tagPillComponent.items,
     {
@@ -32,3 +41,4 @@ tagPillButton?.addEventListener('click', () => {
     },
   ];
 });
+
