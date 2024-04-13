fbff2758f33a9d998048948ba31ff077cd01dabc
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:49 +0300

    comment

--- site/ko/tags/tags.11tydata.js
+++ site/ko/tags/tags.11tydata.js
@@ -1,20 +1,12 @@
-/*
- * Copyright 2021 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Import the `locale` object from the ko.11tydata module. This object likely contains
+// data related to the Korean locale, such as translations or other locale-specific settings.
 const {locale} = require('../ko.11tydata');
+
+// Import the `tags11tyData` function from the tags-11tydata utility module. This function
+// generates 11ty data for tags, using the provided locale object to provide any necessary
+// translations or locale-specific formatting.
 const tags11tyData = require('../../_utils/tags-11tydata');
 
+// Call the `tags11tyData` function with the `locale` object as its argument, and export the
+// result. This will generate 11ty data for tags using the Korean locale.
 module.exports = tags11tyData(locale);
