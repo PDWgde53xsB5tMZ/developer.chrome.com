cc8c35832789a0e7838691decbc97a8a08e6e563
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:46 +0300

    comment

--- tools/devtools/wndt-outline-bootstrap.mjs
+++ tools/devtools/wndt-outline-bootstrap.mjs
@@ -1,14 +1,23 @@
-import {config} from 'dotenv';
+// Import the 'config' object from the 'dotenv' package
+import { config } from 'dotenv';
 
+// Initialize the 'config' object to load environment variables from a .env file
 config();
 
+// Check if the 'DEVTOOLS_VERSION' environment variable is set
 if (!process.env.DEVTOOLS_VERSION) {
+  // If it's not set, throw an error message asking the user to create a .env file with the 'DEVTOOLS_VERSION' parameter
   throw 'Please make sure have a .env file with parameter DEVTOOLS_VERSION="xxx".';
 }
 
-import {createWndtOutline, locales} from './utils.mjs';
+// Import the 'createWndtOutline' function and the 'locales' array from the 'utils.mjs' module
+import { createWndtOutline, locales } from './utils.mjs';
 
+// Extract the list of languages from the 'locales' array
 const languages = locales.map(x => x.lang);
 
+// Ignore TypeScript type checking for the 'createWndtOutline' function call
 // @ts-ignore
+
+// Call the 'createWndtOutline' function with the 'DEVTOOLS_VERSION' environment variable and the list of languages
 createWndtOutline(process.env.DEVTOOLS_VERSION, languages);
