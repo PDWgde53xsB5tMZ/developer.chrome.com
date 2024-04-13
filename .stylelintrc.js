8ad2339d944731f758f93e9ab83ea6c8314e8789
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:56:18 +0300

    comment

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,19 +1,34 @@
-// Tell stylelint to lint using BEM syntax
-// This is some next level regex right here 😅
-// https://github.com/bjankord/stylelint-config-sass-guidelines/issues/20
-const BEM_PATTERN = '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$';
+// Import the stylelint-config-sass-guidelines configuration as a base
+// for this configuration file. This will ensure that we follow best
+// practices for writing Sass using stylelint.
+extends: 'stylelint-config-sass-guidelines',
 
+// Define a set of rules that will be used to lint this configuration file.
+// These rules will be applied on top of the rules defined in the
+// stylelint-config-sass-guidelines configuration.
 module.exports = {
-  extends: 'stylelint-config-sass-guidelines',
+  // Define a set of rules that will be used to lint this configuration file.
   rules: {
-    // Allow a max nesting depth of 2
+    // Limit the maximum nesting depth of selectors to 2. This helps
+    // to keep our Sass code organized and maintainable.
     'max-nesting-depth': 2,
 
+    // Define a regular expression pattern that selectors must match
+    // in order to be considered valid. This pattern is based on the
+    // BEM (Block Element Modifier) methodology, which is a popular
+    // way to write maintainable and scalable Sass code.
     'selector-class-pattern': BEM_PATTERN,
+
+    // Define a regular expression pattern that placeholder variables
+    // must match in order to be considered valid. This pattern is
+    // based on the BEM methodology, which helps to keep our Sass code
+    // organized and maintainable.
     'scss/percent-placeholder-pattern': BEM_PATTERN,
 
-    // Allow selectors like input[type=button]
-    // but not input.foo or input#foo
+    // Allow selectors that use the attribute qualifier, such as
+    // input[type=button], but disallow selectors that use the class
+    // or id qualifiers, such as input.foo or input#foo. This helps
+    // to ensure that our selectors are specific and predictable.
     'selector-no-qualifying-type': [
       true,
       {
