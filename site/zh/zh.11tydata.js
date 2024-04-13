1baff94f6af13393852e617eda68e14349eaaecf
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:34 +0300

    comment

--- site/zh/zh.11tydata.js
+++ site/zh/zh.11tydata.js
@@ -1,3 +1,7 @@
+// This module exports an object with a single property `locale` set to `'zh'`.
+// The `locale` property specifies the language and regional settings to be used
+// for this application, in this case, Chinese.
 module.exports = {
   locale: 'zh',
 };
+
