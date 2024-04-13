da66c3c9942199104fb672d12051a5eaee891576
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:43 +0300

    comment

--- tests/site/_collections/directory.js
+++ tests/site/_collections/directory.js
@@ -1,8 +1,9 @@
-const test = require('ava');
-const {mergeCollections} = require('../../../site/_collections/directory');
+const test = require('ava'); // Importing the Ava testing framework
+const {mergeCollections} = require('../../../site/_collections/directory'); // Importing the mergeCollections function
 
 class FakeCollectionItem {
   constructor(title, fileSlug, date) {
+    // Constructor for creating a fake collection item with title, fileSlug, and date properties
     this.title = title;
     this.fileSlug = fileSlug;
     this.date = new Date(date);
@@ -10,6 +11,7 @@ class FakeCollectionItem {
 }
 
 test('translated item replaces base item', t => {
+  // Test case: Check if a translated item from the localeCollection replaces the corresponding item in baseCollection
   const baseCollection = [
     new FakeCollectionItem('Article 2', 'article-2', '2022-10-07'),
     new FakeCollectionItem('Article 1', 'article-1', '2022-10-06'),
@@ -21,12 +23,14 @@ test('translated item replaces base item', t => {
 
   const mergedCollection = mergeCollections(baseCollection, localeCollection);
 
+  // Check if the mergedCollection contains the expected items
   t.is(mergedCollection[0].fileSlug, localeCollection[0].fileSlug);
   t.is(mergedCollection[0].title, localeCollection[0].title);
   t.is(mergedCollection[1].fileSlug, baseCollection[1].fileSlug);
 });
 
 test('remaining items get appended', t => {
+  // Test case: Check if the remaining items from the baseCollection get appended to the mergedCollection
   const baseCollection = [
     new FakeCollectionItem('Article 4', 'article-4', '2022-10-09'),
     new FakeCollectionItem('Article 3', 'article-3', '2022-10-08'),
@@ -40,9 +44,7 @@ test('remaining items get appended', t => {
 
   const mergedCollection = mergeCollections(baseCollection, localeCollection);
 
+  // Check if the mergedCollection contains the expected items
   t.is(mergedCollection[0].fileSlug, baseCollection[0].fileSlug);
   t.is(mergedCollection[1].fileSlug, baseCollection[1].fileSlug);
-  t.is(mergedCollection[2].fileSlug, baseCollection[2].fileSlug);
-  t.is(mergedCollection[3].fileSlug, localeCollection[0].fileSlug);
-  t.is(mergedCollection[3].title, localeCollection[0].title);
-});
+  t.is(mergedCollection[2].fileSlug, baseCollection[2].fileSlug
