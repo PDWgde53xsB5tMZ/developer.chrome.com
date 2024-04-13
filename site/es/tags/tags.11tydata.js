70ee815e409a586eb0158baf73a0d5c99433c188
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:02:03 +0300

    comment

--- site/es/tags/tags.11tydata.js
+++ site/es/tags/tags.11tydata.js
@@ -1,20 +1,12 @@
-/*
- * Copyright 2020 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+// Import the 'locale' object from '../es.11tydata' module
 const {locale} = require('../es.11tydata');
+
+// Import the 'tags11tyData' function from '../../_utils/tags-11tydata' module
 const tags11tyData = require('../../_utils/tags-11tydata');
 
+// Call the 'tags11tyData' function with the 'locale' object as its argument
+// and export the result
 module.exports = tags11tyData(locale);
+
+// This code is distributed under the Apache License, Version 2.0
+// See https://www.apache.org/licenses/LICENSE-2.0 for details
