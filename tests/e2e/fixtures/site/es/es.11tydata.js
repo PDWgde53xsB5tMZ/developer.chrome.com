5eae42cd022d7d68791f5f1b3669303a727a275f
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 09:03:36 +0300

    comment

--- tests/e2e/fixtures/site/es/es.11tydata.js
+++ tests/e2e/fixtures/site/es/es.11tydata.js
@@ -1,3 +1,6 @@
+// This module exports an object with a single property `locale` set to the string 'es'.
+// The `locale` property specifies the language and regional settings to be used for this module.
+// In this case, 'es' stands for Spanish (Español).
 module.exports = {
   locale: 'es',
 };
