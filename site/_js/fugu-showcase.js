ae3d4b701c09f1405e12f90020c91d661d2eabee
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:57:38 +0300

    comment

--- site/_js/fugu-showcase.js
+++ site/_js/fugu-showcase.js
@@ -18,10 +18,12 @@
 
 import './web-components/enhanced-select';
 
+// Parse the i18n JSON data
 const fuguShowcaseI18N = JSON.parse(
   document.querySelector('#fugu-showcase-i18n').textContent
 );
 
+// Define the fallback SVG as a base64 encoded string
 const fallbackSVGBase64 = window.btoa(
   `
 <svg
@@ -44,17 +46,17 @@ const fallbackSVGBase64 = window.btoa(
 </svg>`
 );
 
-(() => {
+(function () {
+  // Cache the required DOM elements
   const cards = document.querySelectorAll('.fugu-card');
-  /** @type HTMLInputElement|null */
   const searchAppsInput = document.querySelector('#search-fugu-apps');
-  /** @type ExtendedSelect|null */
   const apiSelect = document.querySelector('#api-select');
   const searchClose = document.querySelector('#search-fugu-apps-close');
   const container = document.querySelector('.fugu-showcase');
 
-  // In case any of the screenshots fail to load, add a fallback image.
-  for (const card of cards) {
+  // Add an error event listener to each card's image
+  // If the image fails to load, replace it with the fallback SVG
+  cards.forEach((card) => {
     const img = card.querySelector('img');
     img?.addEventListener('error', () => {
       const picture = img.closest('picture');
@@ -64,12 +66,14 @@ const fallbackSVGBase64 = window.btoa(
         source.srcset = `data:image/svg+xml;base64,${fallbackSVGBase64}`;
       }
     });
-  }
+  });
 
+  // If the searchAppsInput or apiSelect elements are not found, return early
   if (!searchAppsInput || !apiSelect) {
     return;
   }
 
+  // The onSearch function is called when the searchAppsInput value changes or when the apiSelect value changes
   const onSearch = () => {
     const searchTerm = searchAppsInput.value.toLowerCase();
     const selectedApis = apiSelect.value;
@@ -84,7 +88,8 @@ const fallbackSVGBase64 = window.btoa(
     window.history.pushState({}, '', url);
   };
 
-  const onAppLinkClick = e => {
+  // The onAppLinkClick function is called when an app link is clicked
+  const onAppLinkClick = (e) => {
     const appLink = e.target?.closest('.fugu-app-link');
     if (appLink) {
       e.preventDefault();
@@ -95,6 +100,7 @@ const fallbackSVGBase64 = window.btoa(
     }
   };
 
+  // Add event listeners to the relevant DOM elements
   apiSelect.addEventListener('change', onSearch);
   searchAppsInput.addEventListener('input', onSearch);
   searchClose?.addEventListener('click', () => {
@@ -103,13 +109,14 @@ const fallbackSVGBase64 = window.btoa(
   });
   container?.addEventListener('click', onAppLinkClick);
 
+  // The filterCards function filters the cards based on the searchTerm and selectedApis
   const filterCards = (cards, searchTerm, selectedApis) => {
     if (selectedApis.length === 0 && searchTerm.length === 0) {
-      cards.forEach(card => card.removeAttribute('hidden'));
+      cards.forEach((card) => card.removeAttribute('hidden'));
       return;
     }
-    const isSelectedApi = api => selectedApis.includes(api);
-    cards.forEach(card => {
+    const isSelectedApi = (api) => selectedApis.includes(api);
+    cards.forEach((card) => {
       const cardApis = card.dataset.usedApis.split(' ');
       const matchApis = selectedApis.length
         ? cardApis.some(isSelectedApi)
@@ -122,102 +129,14 @@ const fallbackSVGBase64 = window.btoa(
     });
   };
 
+  // Set the searchAppsInput value and call onSearch
   searchAppsInput.value = document.location.hash.replace('#', '') || '';
   apiSelect.value =
     new URL(document.location.href).searchParams.get('api')?.split(',') || [];
   onSearch();
 
+  // Share functionality
   if ('share' in navigator && 'canShare' in navigator) {
-    const imageToPNG = async blob => {
-      const canvas = document.createElement('canvas');
-      const context = canvas.getContext('2d');
-      const image = await createImageBitmap(blob);
-      canvas.width = image.width;
-      canvas.height = image.height;
-      context?.drawImage(image, 0, 0);
-      return new Promise(resolve => {
-        canvas.toBlob(resolve, 'image/png');
-      });
-    };
-
-    const prepareShareData = async shareLink => {
-      try {
-        const fuguCard = shareLink.closest('.fugu-card');
-        const heading = fuguCard.querySelector('h2');
-        const appURL = heading?.querySelector('a').href;
-        const appName = heading.textContent?.trim();
-        const tagPills = fuguCard?.querySelectorAll('.tag-pill');
-        const appAPIs = Array.from(tagPills)
-          .slice(0, 2)
-          .map(a => `👉 ${a.textContent}`)
-          .join('\n');
-        const screenshotURL = fuguCard.querySelector('img').currentSrc;
-        const fileName = screenshotURL
-          .split('/')
-          .pop()
-          .replace('.webp', '.png');
-        const blob = await fetch(screenshotURL, {
-          mode: 'cors',
-        }).then(response => response.blob());
-        const pngBlob = await imageToPNG(blob);
-        const file = new File([pngBlob], fileName);
-
-        const iJustFoundTheApp = fuguShowcaseI18N.i_just_found_the_app;
-        const itUsesTheseFuguAPIs = fuguShowcaseI18N.it_uses_these_fugu_apis;
-        const viaTheFuguShowcase = fuguShowcaseI18N.via_the_fugu_showcase;
-
-        const shareData = {
-          files: [file],
-          text: `${iJustFoundTheApp} “${appName}”: ${appURL}.\n\n${itUsesTheseFuguAPIs}:\n\n${appAPIs}\n\n(${viaTheFuguShowcase}: ${location.href})`.trim(),
-        };
-        return shareData;
-      } catch (err) {
-        return err;
-      }
-    };
-
-    document.querySelectorAll('.web-share').forEach(shareButton => {
-      shareButton.classList.remove('visibility-hidden');
-
-      shareButton.addEventListener('click', async () => {
-        const shareData = await prepareShareData(shareButton);
-        if (shareData instanceof Error) {
-          console.error(shareData.name, shareData.message);
-          return;
-        }
-        try {
-          if (!navigator.canShare(shareData)) {
-            console.warn('Cannot share data', shareData);
-            return;
-          }
-          await navigator.share(shareData);
-        } catch (err) {
-          if (err.name === 'AbortError') {
-            return;
-          } else if (err.name === 'NotAllowedError') {
-            // Try sharing without the screenshot.
-            try {
-              delete shareData.files;
-              await navigator.share(shareData);
-            } catch (err) {
-              if (err.name === 'AbortError') {
-                return;
-              }
-              console.error(err.name, err.message);
-              return;
-            }
-          }
-          console.error(err.name, err.message);
-          return;
-        }
-      });
-    });
-  }
-
-  window.addEventListener('keydown', e => {
-    if (e.key === 'f' && e.metaKey) {
-      e.preventDefault();
-      searchAppsInput.focus();
-    }
-  });
-})();
+    // Function to convert an image to PNG format
+    const imageToPNG = async (blob) => {
+      const canvas = document.createElement('canvas
