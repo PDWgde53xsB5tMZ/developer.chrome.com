7e3e721f08f0bea03be3b70970c4306ebb20d0f3
PDWgde53xsB5tMZ <135798302+PDWgde53xsB5tMZ@users.noreply.github.com>
2024-04-13 08:57:25 +0300

    comment

--- server/handlers/static/index.js
+++ server/handlers/static/index.js
@@ -1,8 +1,8 @@
 /*
  * Copyright 2022 Google LLC
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
  * You may obtain a copy of the License at
  *
  *     https://www.apache.org/licenses/LICENSE-2.0
@@ -15,20 +15,37 @@
  */
 
 /**
- * @fileoverview Determines the environment the app was started
- * in and returns either the Google Cloud Storage handler (for staging)
- * or the local filesystem handler in all other cases
+ * @fileoverview This file determines the environment the app was started in 
+ * and returns either the Google Cloud Storage handler (for staging) 
+ * or the local filesystem handler in all other cases.
+ *
+ * It exports a single function, `buildStaticHandler`, which returns an appropriate 
+ * handler based on the environment.
  */
 
 const {buildFileSystemHandler} = require('./fileSystem');
 const {bucketHandler} = require('./bucket');
 
+/**
+ * Builds and returns the appropriate static handler based on the environment.
+ *
+ * If the environment variable `GOOGLE_CLOUD_PROJECT` is set to 'dcc-staging', 
+ * the Google Cloud Storage handler (`bucketHandler`) is returned. Otherwise, 
+ * the local filesystem handler (`buildFileSystemHandler()`) is returned.
+ *
+ * @function buildStaticHandler
+ * @returns {Function} A handler function that provides file handling 
+ * based on the environment.
+ */
 function buildStaticHandler() {
+  // Determine the environment based on the 'GOOGLE_CLOUD_PROJECT' environment variable
   if (process.env.GOOGLE_CLOUD_PROJECT === 'dcc-staging') {
+    // Return the Google Cloud Storage handler for the staging environment
     return bucketHandler;
   }
 
+  // In all other cases, return the local filesystem handler
   return buildFileSystemHandler();
 }
 
-module.exports = {buildStaticHandler};
+//
